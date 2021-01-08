983b4e2836de10a0862a23a86e68b900dfa706b5
Stephen Price <stephen_lprice@live.com>
2021-01-08 02:27:18 -0600

    changed to dynamic loading

--- assets/script.js
+++ assets/script.js
@@ -34,76 +34,223 @@ init();
 $(document).ready(function() {
     // DOM elements
     var startBtn = $("#startBtn");
+    var timeOut = 1000;
 
 
     // When a user click on Start Game, load the first question randomly.
     function startQuiz() {
-        randomizer();
+        q1();
     }
 
-    function randomizer() {
-        // Separate store for the questions object
-        var qObj = questions;
-        var ranQ = Math.floor(Math.random() * 5 + 1);
-        for (question in qObj) {
-            if (Object.keys(qObj).indexOf(question) === ranQ) {
-                content.empty();
-                var answer = qObj[question].correct;
-                content.append( /*html*/ `
-                    <h1>${qObj[question].question}</h1><br>
-                    <div id="answers" class="container">
-                        <div id="answer-buttons" class="class row">
-                            <div class="div col-12 col-sm-6 col-md-3 my-3">
-                                <button type="button" class="btn btn-outline-primary btn-lg">${qObj[question].answer1}</button>
-                            </div>
-                            <div class="div col-12 col-sm-6 col-md-3 my-3">
-                                <button type="button" class="btn btn-outline-primary btn-lg">${qObj[question].answer2}</button>
-                            </div>
-                            <div class="div col-12 col-sm-6 col-md-3 my-3">
-                                <button type="button" class="btn btn-outline-primary btn-lg">${qObj[question].answer3}</button>
-                            </div>
-                            <div class="div col-12 col-sm-6 col-md-3 my-3">
-                                <button type="button" class="btn btn-outline-primary btn-lg">${qObj[question].answer4}</button>
-                            </div>
-                        </div>
-                        <p id="message"></p>
+    function q1(){
+        content.empty();
+        var correct = "alerts";
+        content.append( /*html*/ `
+            <h1>Commonly used data types DO NOT include:</h1><br>
+            <div id="answers" class="container">
+                <div id="answer-buttons" class="class row">
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">strings</button>
                     </div>
-                `);
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">booleans</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">alerts</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">numbers</button>
+                    </div>
+                </div>
+                <p id="message"></p>
+            </div>
+        `);
 
-                $(":button").on("click", function(b){
-                    if (b.target && b.target.nodeName == "BUTTON") {
-                        if (this.textContent == answer) {
-                            $("#message").text("Your Answer is Correct!");
-                            setTimeout(delete qObj[question], 100);
-                            setTimeout(randomizer(), 100); 
-                        }
-                        else {
-                            $("#message").text("Your Answer is Incorrect!");
-                            setTimeout(delete qObj[question], 100);
-                            setTimeout(randomizer(), 100);
-                        }
-                    }
-
-                    
-                });
-
+        $(":button").on("click", function(b){
+            if (b.target && b.target.nodeName == "BUTTON") {
+                if (this.textContent == correct) {
+                    success();
+                    setTimeout(q2, timeOut);
+                }
+                else {
+                    failure();
+                    setTimeout(q2, timeOut);
+                }
             }
-        }
+        });
+    }
+
+    function q2(){
+        content.empty();
+        var correct = "parentheses";
+        content.append( /*html*/ `
+            <h1>The condition in an if / else statement is enclosed within ___.</h1><br>
+            <div id="answers" class="container">
+                <div id="answer-buttons" class="class row">
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">quotes</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">curly brackets</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">parentheses</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">square brackets</button>
+                    </div>
+                </div>
+                <p id="message"></p>
+            </div>
+        `);
+
+        $(":button").on("click", function(b){
+            if (b.target && b.target.nodeName == "BUTTON") {
+                if (this.textContent == correct) {
+                    success();
+                    setTimeout(q3, timeOut);
+                }
+                else {
+                    failure();
+                    setTimeout(q3, timeOut);
+                }
+            }
+        });
+    }
+
+    function q3(){
+        content.empty();
+        var correct = "all of the above";
+        content.append( /*html*/ `
+            <h1>Arrays in Javascript can be used to store ____.</h1><br>
+            <div id="answers" class="container">
+                <div id="answer-buttons" class="class row">
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">numbers and strings</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">other arrays</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">booleans</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">all of the above</button>
+                    </div>
+                </div>
+                <p id="message"></p>
+            </div>
+        `);
+
+        $(":button").on("click", function(b){
+            if (b.target && b.target.nodeName == "BUTTON") {
+                if (this.textContent == correct) {
+                    success();
+                    setTimeout(q4, timeOut);
+                }
+                else {
+                    failure();
+                    setTimeout(q4, timeOut);
+                }
+            }
+        });
+    }
+
+    function q4(){
+        content.empty();
+        var correct = "quotes";
+        content.append( /*html*/ `
+            <h1>String values must be enclosed within ___ when being assigned to variables</h1><br>
+            <div id="answers" class="container">
+                <div id="answer-buttons" class="class row">
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">commas</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">curly brackets</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">quotes</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">parentheses</button>
+                    </div>
+                </div>
+                <p id="message"></p>
+            </div>
+        `);
+
+        $(":button").on("click", function(b){
+            if (b.target && b.target.nodeName == "BUTTON") {
+                if (this.textContent == correct) {
+                    success();
+                    setTimeout(q5, timeOut);
+                }
+                else {
+                    failure();
+                    setTimeout(q5, timeOut);
+                }
+            }
+        });
+    }
+
+    function q5(){
+        content.empty();
+        var correct = "console.log";
+        content.append( /*html*/ `
+            <h1>A useful tool for development and debugging, used to print content to the debugger is:</h1><br>
+            <div id="answers" class="container">
+                <div id="answer-buttons" class="class row">
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">JavaScript</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">terminal/bash</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">for loops</button>
+                    </div>
+                    <div class="div col-12 col-sm-6 col-md-3 my-3">
+                        <button type="button" class="btn btn-outline-primary btn-lg">console.log</button>
+                    </div>
+                </div>
+                <p id="message"></p>
+            </div>
+        `);
+
+        $(":button").on("click", function(b){
+            if (b.target && b.target.nodeName == "BUTTON") {
+                if (this.textContent == correct) {
+                    success();
+                    // setTimeout(q2, timeOut);
+                }
+                else {
+                    failure();
+                    // setTimeout(q2, timeOut);
+                }
+            }
+        });
+    }
+
+    function success() {
+        content.append( /*html*/ `
+            <br><br>
+            <div class="alert alert-success" role="alert">
+                Your Answer is Correct!
+            </div>
+        `);
+    }
+
+    function failure() {
+        content.append( /*html*/ `
+            <br><br>
+            <div class="alert alert-danger" role="alert">
+                Your Answer is Wrong!
+            </div>
+        `);
     }
 
     // Event handlers
     startBtn.on("click", startQuiz);
 });
 
-// function evaluator(answer) {
-//     if ($(":button").textContent === answer) {
-//         $("#message").text("Your Answer is Correct!");
-//         setTimeout(delete qObj[question], 100);
-//         setTimeout(randomizer(), 100); 
-//     }
-//     else {
-//         $("#message").text("Your Answer is Incorrect!");
-//         setTimeout(delete qObj[question], 100);
-//         setTimeout(randomizer(), 100);
-//     }
-// }
